ad906985ebffadea6fac4368b5572586e96cbe91
Romkappa <romuald.lecorroller@gmail.com>
2025-12-15 15:40:45 +0100

    20251212: NEW People front/redis
    - Modification de l'affichage de la page detail d'un perso.
    - Ajout de bouton popur revenir a la page precedente ou home.
    - Ajout de redis pour un rechargement de page car ca prenait trop de temps chaque modif de code.

--- peoples/views.py
+++ peoples/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
+from django.core.cache import cache
 
 # Create your views here.
 from django.http import JsonResponse
 from .services import fetch_people_data_by_id, fetch_list_by_resource
 
 def people_detail(request, people_id):
-    people = fetch_people_data_by_id(people_id)
-    if not people:
-        return JsonResponse({'error': 'People not found'}, status=404)
+    cache_key = "people_detail_{}".format(people_id)
+
+    people = cache.get(cache_key)
+    if people is None:
+        print("🔴 Appel API SWAPI")
+        people = fetch_people_data_by_id(people_id)
+
+        if not people:
+            return JsonResponse(
+                {"error": "People not found"},
+                status=404
+            )
+
+        cache.set(cache_key, people, timeout=60 * 60) # Cache for 1 hour
+    else:
+        print("🟢 Données depuis Redis")
+    
     return render(request, "people_detail.html", {"people": people})
 
 def people_list(request):
-    people_list = fetch_list_by_resource("people")
-    if not people_list:
-        return JsonResponse({'error': 'People list not found'}, status=404)
-    return render(request, "people_list.html", {"people_list": people_list})
+    cache_key = "people_list"
 
+    people_list = cache.get(cache_key)
+
+    if people_list is None:
+        print("🔴 Appel API SWAPI")
+        people_list = fetch_list_by_resource("people")
+
+        if not people_list:
+            return JsonResponse(
+                {"error": "People list not found"},
+                status=404
+            )
+
+        cache.set(cache_key, people_list, timeout=60 * 60) # Cache for 1 hour
+    else:
+        print("🟢 Données depuis Redis")
+
+    return render(request, "people_list.html", {"people_list": people_list})
--- star_wars_api/settings.py
+++ star_wars_api/settings.py
@@ -128,3 +128,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "swapi",
+    }
+}
